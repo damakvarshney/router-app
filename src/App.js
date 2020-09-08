c8c58cdd8015ade24dbbd89ffab38f17681bd01a
damakvarshney <damakvarshney@gmail.com>
2020-09-08 10:18:34 +0530

    8/9/20 10:18

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/navbar.jsx";
+import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-import { Route } from "react-router-dom";
+// import ProductDetails from "./components/productDetails";
+// import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <div>
-          <Route path="/" component={Home} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
+        <div className="content">
+          <Switch>
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
         </div>
       </div>
     );
